97fa2bacd6ffd493b5573e08bcda536ab6fccb2e
LouisonSERVANT <145564515+LouisonSERVANT@users.noreply.github.com>
2023-12-11 09:48:27 +0100

    feat(origin)

--- entity/Monster.java
+++ entity/Monster.java
@@ -1,5 +1,10 @@
 package entity;
 
 public class Monster {
-    
+    private String name;
+    private Type type;
+    private int hp;
+    private int speed;
+    private int attack;
+    private int defense;
 }
